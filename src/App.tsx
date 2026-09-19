9e5ab17d752070f3e103ce7dc6b89ac4c5f28628
tjdud9329 <tjdud9329@gmail.com>
2026-09-19 22:33:51 +0900

    [Feat] react-router 기반 기본 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import LoginPage from './pages/LoginPage';
+
+import HomePage from './pages/HomePage';
+import MyPage from './pages/MyPage';
+
 function App() {
   return (
-    <>
-      <div className="text-red-400">초기 설정 완료</div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="/mypage" element={<MyPage />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/MyPage.tsx
@@ -0,0 +1,5 @@
+const MyPage = () => {
+  return <div>로그인</div>;
+};
+
+export default MyPage;
